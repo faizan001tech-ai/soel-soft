3a1734ff37973b9c78b2f3ce1e95427e8cc1bf45
Faizanjanhangir <faizan001.tech@gmail.com>
2026-04-18 00:53:45 -0700

    Removed Hero section from homepage

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,4 +1,3 @@
-import Hero from '../components/Hero';
 import CategorySection from '../components/CategorySection';
 import FeaturedProducts from '../components/FeaturedProducts';
 import BestSellers from '../components/BestSellers';
@@ -8,7 +7,6 @@ import Testimonials from '../components/Testimonials';
 const Home = () => {
   return (
     <div>
-      <Hero />
       <CategorySection />
       <FeaturedProducts />
       <PromoBanner />
